Changed correct units for FLG (ul/min) vs SLS (ml/min)

Modules/FLG_M_Plus.py
@@ -40,7 +40,7 @@ class MicroFlowMeter:
         if save_data:
             print(len(time_list))
             print(len(flow_rate_list))
-            np.savetxt(filename, np.c_[time_list, flow_rate_list], delimiter=',', header='s,mL/min', comments='')
+            np.savetxt(filename, np.c_[time_list, flow_rate_list], delimiter=',', header='s,uL/min', comments='')
             
         return flow_rate_list,time_list
 

Modules/Plot_Client.py
@@ -29,7 +29,7 @@ class Plot:
         else:
             print("FLG Plotting")
             file_pattern = 'flg_flow_rate_forward_*.csv'
-            search_pattern = r'sls_flow_rate_forward_(-?\d+\.\d+)_ul_min'
+            search_pattern = r'flg_flow_rate_forward_(-?\d+\.\d+)_ul_min'
             files = glob.glob(os.path.join(self.directory, file_pattern))
 
         for i, filename in enumerate(files):
@@ -43,9 +43,9 @@ class Plot:
                 sys.exit(1)
 
             df = pd.read_csv(filename)
-            mL_min = (df['mL/min']).tolist()
-            # print(mL_min)
-            s = (df['ms']).tolist()
+            mL_min = df['mL/min'].tolist() if self.SLS1500_flag else [(value / 1000) for value in df['uL/min'].tolist()]
+            s = [(value / 1000) for value in df['ms'].tolist()] if self.SLS1500_flag else df['s'].tolist()
+
             s = np.array(s)
             low_filter = s > 1
             q = np.array(mL_min)
@@ -95,7 +95,7 @@ class Plot:
         else:
             print("FLG Plotting")
             file_pattern = 'flg_flow_rate_forward_*.csv'
-            search_pattern = r'sls_flow_rate_forward_(-?\d+\.\d+)_ul_min'
+            search_pattern = r'flg_flow_rate_forward_(-?\d+\.\d+)_ul_min'
             files = glob.glob(os.path.join(self.directory, file_pattern)) 
             
         colors = cm.viridis(np.linspace(0.1,0.9, len(files)))
@@ -110,8 +110,8 @@ class Plot:
                 sys.exit(1)
 
             df = pd.read_csv(filename)
-            mL_min = (df['mL/min']).tolist()
-            s = (df['ms']).tolist()
+            mL_min = df['mL/min'].tolist() if self.SLS1500_flag else [(value / 1000) for value in df['uL/min'].tolist()]
+            s = [(value / 1000) for value in df['ms'].tolist()] if self.SLS1500_flag else df['s'].tolist()
             ax.plot(s, mL_min, label='q measured {} mL/min'.format(flow_rate / 1000), c= colors[i])
 
         ax.autoscale(axis='y')
@@ -155,6 +155,6 @@ class Plot:
         
 
 if __name__=="__main__":
-    plot_module = Plot(SLS1500_flag = True)
+    plot_module = Plot(SLS1500_flag = False)
     plot_module.q_vs_qs_and_relative_error()
     plot_module.flow_rate_over_time()

Modules/flow_calibration.py
@@ -8,7 +8,7 @@ def create_folder(path):
         os.mkdir(path)
 from SLS_1500 import *
 from FLG_M_Plus import *
-from Plot_Client import Plot
+from Plot_Client import *
 
 def experiment_name(Sls1500_flag,):
     pass
@@ -30,10 +30,7 @@ def experiment(flow_rates,pump,syringe,runtime=None,flow_meter=None,SL1500_flag=
 
 # Glycerol Test
 # --------------------
-# SLS (Flow duration of 10 s-step)
-# -20-2mL/min steps: 0.5mL/min
 # FLG (Flow duration of  20 seconds)
-# 0.1- 2 mL/min stps 0.1mL/min ! wil need a 1ml syringe which can be found on the fifth floor!)
 # 0.01 0 0.1 ml/min stps 0.010 mL/min stps
 # see if you can make a lookup table from qmeas and qset
 # see if it can record forward and back flow and if so repeat the top test in the negative space
@@ -41,21 +38,18 @@ def experiment(flow_rates,pump,syringe,runtime=None,flow_meter=None,SL1500_flag=
 # integrate the delta p (v) curve
 
 if __name__=="__main__":
-    SLS1500_flag = True
-    syringe_name = "A"
-    syringe_volume = "60"
+    SLS1500_flag = False
+    syringe_name = "B"
+    syringe_volume = "1" # mL
     syringe_type = "bdp"
-    runtime_s = 10  # Set the runtime value to 10 seconds
-    start_value = np.array([-20.1])  # First value of appended to flow rates in mL/min
-    flow_range = np.arange(-20, -8.1, 0.5)  # Flow Rate ranges and step size in mL/min
-    end_value = np.array([-2.1])  # First value of appended to flow rates in mL/min
+    runtime_s = 20  # Set the runtime value to 10 seconds
+    start_value = np.array([0.0])  # First value of appended to flow rates in mL/min
+    flow_range = np.arange(0.01, 0.11, 0.01)  # Flow Rate ranges and step size in mL/min
+    end_value = np.array([0.12])  # First value of appended to flow rates in mL/min
+
     pump_flow_rates = np.hstack((start_value, flow_range,end_value)) * 1000  # Concatenate x1 and x2 arrays, then multiply by 1000 (ul/min)
     pump_flow_rates = pump_flow_rates[::-1]
     print("FLow Rates [uL/min]: {}".format(pump_flow_rates))
-    
-
-    
-
     volume = pump_flow_rates * runtime_s / (60*1000)  # Calculate the volume based on pump flow rates and runtime (ml)
     vtot = np.sum(volume)  # Calculate the total volume by summing all elements in the volume array
     minutes_tot = len(pump_flow_rates) * runtime_s //60
@@ -69,38 +63,22 @@ if __name__=="__main__":
     json_file = os.path.join("Parameters.json")
     with open(json_file, 'w') as fp:
         json.dump(input_dic, fp)
-
         
-    syringeA = Syringe("A", '60', 'bdp')  # Create a Syringe object with syringe number 'a', volume '10 ml', and plastic type 'bdp'
-    # syringeB = Syringe("B", '1', 'bdp')  # Create a Syringe object with syringe number 'b', volume '10 ml', and plastic type 'bdp'
-
-    pumpA = Pump("A", "COM5", syringe1=syringeA)  # Create a Pump object with pump number 'A', connected to serial port 'COM5', and using syringeA
-    # pumpB = Pump("B", "COM5", syringe1=syringeB)  # Create a Pump object with pump number 'B', connected to serial port 'COM', and using syringeB
-    
-    pumpA.stop()  # Stop the pump before performing any operations
-    # pumpB.stop()  # Stop the pump before performing any operations
-
+    syringe = Syringe(syringe_name, syringe_volume, syringe_type=syringe_type)  # Create a Syringe object with syringe number 'a', volume '10 ml', and plastic type 'bdp'
+    pump = Pump(syringe_name, "COM5", syringe1=syringe)  # Create a Pump object with pump number 'B', connected to serial port 'COM', and using syringeB
+    pump.stop()  # Stop the pump before performing any operations
 
     if SLS1500_flag:
         port = ShdlcSerialPort(port='COM3', baudrate=115200)  # Create a serial port object for communication on port 'COM3' with baudrate 115200
         flow_meter = SLS_1500Device(ShdlcConnection(port), slave_address=0)  # Create a flow meter device using the ShdlcConnection and the port, with a slave address of 0
-        # Call the experiment function with the specified parameters
-        experiment(flow_rates=pump_flow_rates, pump=pumpA, syringe=syringeA, runtime=runtime_s, flow_meter=flow_meter)
+        # experiment(flow_rates=pump_flow_rates, pump=pumpA, syringe=syringeA, runtime=runtime_s, flow_meter=flow_meter)
     
     else:
         microFlowMeter = MicroFlowMeter()
-        # experiment(flow_rates=pump_flow_rates, pump=pumpB, syringe=syringeB, runtime=runtime_s, flow_meter=microFlowMeter, SL1500_flag=False)
-
-    pumpA.stop()  # Stop the pump after the experiment is completed
-    # pumpB.stop()  # Stop the pump after the experiment is completed
-    
-    
+        experiment(flow_rates=pump_flow_rates, pump=pump, syringe=syringe, runtime=runtime_s, flow_meter=microFlowMeter, SL1500_flag=False)
+    pump.stop()  # Stop the pump after the experiment is completed
+   
     plot = Plot(SLS1500_flag)
     plot.flow_rate_over_time()
     plot.q_vs_qs_and_relative_error()
     
-
-
-    
-
-