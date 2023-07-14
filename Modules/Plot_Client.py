4bb8f09bf22bd0c904b340ccec220e8f19fab0a7
Christopher Stocker <julienstocker@icloud.com>
2023-07-14 15:25:35 -0400

    Update Plot _Client now prints all SLS  Water q_vs_qs

--- Modules/Plot_Client.py
+++ Modules/Plot_Client.py
@@ -10,13 +10,134 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from natsort import natsorted
 
+SLS=1
+FLG=2
+WATER=3
+GLYCEROL=4
+
+def get_path_case(case, subcase):
+    print("Getting path")
+    switch = {
+        SLS: {
+            WATER: ('Data_Calibration\\water\\sls',r'Data_Calibration\\water\\sls\\.*\\sls_flow_rate_forward_(-?\d+\.\d+)_ul_min', "Medium: Water - SLS 1500"),
+            GLYCEROL:('Data_Calibration\\glycerol\\sls',r'Data_Calibration\\glycerol\\sls\\.*\\sls_flow_rate_forward_(-?\d+\.\d+)_ul_min', "Medium: Glycerol - SLS 1500")
+        },
+        FLG: {
+            WATER: ('Data_Calibration\\water\\flg', r'Data_Calibration\\water\\flg\\.*\\flg_flow_rate_forward_(-?\d+\.\d+)_ul_min', "Medium: Water - FLG 1500"),
+            GLYCEROL: ('Data_Calibration\\glycerol\\flg', r'Data_Calibration\\glycerol\\flg\\.*\\flg_flow_rate_forward_(-?\d+\.\d+)_ul_min', "Medium: Glycerol - FLG 1500")
+        }
+    }
+    return switch.get(case, {}).get(subcase, 'Invalid case or subcase')
+
 class Plot:
-    def __init__(self,SLS1500_flag ):
+    def __init__(self,SLS1500_flag=None ):
         self.directory = os.path.dirname(os.path.abspath(__file__)) 
         self.SLS1500_flag = SLS1500_flag
+    
+    def set_search_directory(self,SLS1500_flag,water_flag):
+        pass
+
+
+    def all_q_vs_qs_case(self, case:int, sub_case:int):
+        q_set_list = []
+        q_measured_list = []
+        q_std_list = []
+        q_percent_error_list = []
+        folder_path, match_pattern ,plot_title = get_path_case(case, sub_case)
+
+        # match_pattern = 'Data_Calibration\\water\\sls\\*\\sls_flow_rate_forward_(-?\d+\.\d+)_ul_min'
         
+
+        print("All q vs. qs")
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                if file.endswith('.csv'):
+                    file_path = os.path.join(root, file)
+                    # print(file_path)
+                    df = pd.read_csv(file_path)
+                    mL_min= df['mL/min'].tolist() 
+                    s = [(value / 1000) for value in df['ms'].tolist()] 
+                    
+                    s = np.array(s)
+                    # print(s)
+                    low_filter = s > 2
+                    q = np.array(mL_min)
+                    q = q[low_filter]
+                    
+                    match = re.search(match_pattern, file_path)
+                    if match:
+                        flow_rate = float(match.group(1))
+                        # print(flow_rate)
+                    else:
+                        print("Error: Could not parse file name: {}".format(file_path))
+                        sys.exit(1)
+
+                    q_average = np.average(q)
+                    q_error = abs(q_average - flow_rate / 1000) / (flow_rate / 1000) * 100
+                    q_percent_error_list.append(q_error)
+
+                    q_std = np.std(q)
+                    q_measured_list.append(q_average)
+                    q_std_list.append(q_std)
+                    q_set_list.append(flow_rate / 1000)
+                
+        plt.figure(figsize=(10, 5)) # Fix to make plots appear larger
+
+        # Plot measured vs. set flow rate
+        plt.subplot(1, 2, 1)
+        plt.errorbar(q_set_list, q_measured_list, yerr=q_std_list, fmt='o', capsize=5, label='Measured')
+        plt.plot(q_set_list, q_set_list, label='Set', linestyle='--')
+        plt.xlabel("Set Flow Rate (mL/min)")
+        plt.ylabel("Measured Flow Rate (mL/min)")
+        plt.title("Flow Rate Measurement")
+        plt.legend()
+
+        # Remove right and top spines
+        plt.gca().spines['right'].set_visible(False)
+        plt.gca().spines['top'].set_visible(False)
+
+        # Plot percent error
+        plt.subplot(1, 2, 2)
+        plt.plot(q_set_list, q_percent_error_list, 'o')
+        plt.xlabel("Set Flow Rate (mL/min)")
+        plt.ylabel("Percent Error (%)")
+        plt.title("Flow Rate Measurement - Percent Error")
+
+        # Remove right and top spines
+        plt.gca().spines['right'].set_visible(False)
+        plt.gca().spines['top'].set_visible(False)
+
+        # Add a title for the entire figure
+        plt.suptitle(plot_title, fontsize=16)
+
+        plt.tight_layout()
+        plt.show()
+            
+
+
+        
+                    # # # sys.exit(1)
+
+                    # match = re.search(search_pattern, file_path)
+                    # if match:
+                    #     flow_rate = float(match.group(1))
+                    # else:
+                    #     print("Error: Could not parse file name: {}".format(file_path))
+                    #     sys.exit(1)
+                    
+                    # df = pd.read_csv(file_path)
+                    # mL_min = df['mL/min'].tolist() if self.SLS1500_flag else [(value / 1000) for value in df['uL/min'].tolist()]
+                    # s = [(value / 1000) for value in df['ms'].tolist()] if self.SLS1500_flag else df['s'].tolist()
+
+                    # print("mL/min: ", mL_min)
+                    # print("s: ", s)
+
+
+        
+            
     # Function to plot measured vs. set flow rate
     def q_vs_qs_and_relative_error(self):
+        assert self.SLS1500_flag is not None, "SLS1500_flag must be set before calling this function"
         q_set_list = []
         q_measured_list = []
         q_std_list = []
@@ -85,6 +206,7 @@ class Plot:
         plt.show()
 
     def flow_rate_over_time(self):
+        assert self.SLS1500_flag is not None, "SLS1500_flag must be set before calling this function"
 
         print("Plotting Flow Rate Over Time")
         fig, ax = plt.subplots(1, 1, figsize=(16, 9))  # Set the figsize to the screen aspect ratio
@@ -155,6 +277,9 @@ class Plot:
 #     return pressure
         
 if __name__=="__main__":
-    plot_module = Plot(SLS1500_flag = True)
-    plot_module.q_vs_qs_and_relative_error()
-    plot_module.flow_rate_over_time()
+    plot = Plot(SLS1500_flag = True)
+    # plot_module.q_vs_qs_and_relative_error()
+    # plot_module.flow_rate_over_time()
+
+    # plot = Plot()
+    plot.all_q_vs_qs_case(SLS,WATER)
