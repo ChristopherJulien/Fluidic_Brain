a94ea75c110b9d013d67f66487727c5805b3505a
Christopher Stocker <julienstocker@icloud.com>
2023-08-02 17:30:24 -0400

    Plotting Arrow classes

--- Actuator/model_vs_export_comparison.py
+++ Actuator/model_vs_export_comparison.py
@@ -3,19 +3,21 @@ import numpy as np
 import math
 import sys
 
-Length = 4.73 #cm
-Width = 0.24 #cm
-angles = np.linspace(-75*(np.pi)/180, 67*(np.pi)/180, 1000)  # Generate x values
+Length = 4.73  # cm
+Width = 0.24  # cm
+angles = np.linspace(-75*(np.pi)/180, 67*(np.pi) /
+                     180, 1000)  # Generate x values
 
-x_offset = 0.0 #cm
-y_offset = 0.0 #cm
+x_offset = 0.0  # cm
+y_offset = 0.0  # cm
+
 
 def get_pos_video(file_path):
     time = []
     x_values = []
     y_values = []
-    x_video_offset = 0.0 #cm'
-    y_video_offset = 0.0 #cm
+    x_video_offset = 0.0  # cm'
+    y_video_offset = 0.0  # cm
 
     with open(file_path, 'r', encoding='utf-8-sig') as file:
         lines = file.readlines()
@@ -34,28 +36,32 @@ def get_pos_video(file_path):
 
     return time, x_values, y_values
 
+
 def get_pos_model_1(angle, L=Length, W=Width):
     R = L/2
     print('Model 1: R =', R)
     return R*np.cos(angle) + x_offset, R*np.sin(angle) + y_offset
-    
+
+
 def get_pos_model_2(angle, L=Length, W=Width):
     R = math.sqrt((L/2)**2 + W**2)
     print('Model 2: R =', R)
     return R*np.cos(angle) + x_offset, R*np.sin(angle) + y_offset
+
 
 def get_pos_model_3(angle, L=Length, W=Width):
     R = math.sqrt((L/2)**2 + (1.5*W)**2)
     print('Model 3: R =', R)
     return R*np.cos(angle) + x_offset, R*np.sin(angle) + y_offset
 
+
 def plot_trajectory_all_models(file_path, angles, L=Length, W=Width):
-    file_path=file_path
+    file_path = file_path
     # Calculate the positions for the three models
     x1, y1 = get_pos_model_1(angles)
     x2, y2 = get_pos_model_2(angles)
     x3, y3 = get_pos_model_3(angles)
-    _,x4, y4 = get_pos_video(file_path)
+    _, x4, y4 = get_pos_video(file_path)
 
     # Create the plot with all three models superimposed
     plt.figure(figsize=(8, 6))
@@ -67,48 +73,78 @@ def plot_trajectory_all_models(file_path, angles, L=Length, W=Width):
     # Set labels and title
     plt.xlabel("X-axis")
     plt.ylabel("Y-axis")
-    plt.title("Superimposed Models")
+    plt.title("Model Trajectories vs Video Trajectory")
 
     # Add a legend
-    legend_text = "Length  {}cm".format(Length)
+    legend_text = "Actuator Length  {}cm".format(Length)
     plt.legend(title=legend_text)
     plt.show()
+
 
 def plot_error_all_models():
     pass
 
-if __name__ == "__main__":
-   file_path = r"C:\Users\Julien\OneDrive - Harvard University\Documents\Fluidic_Brain\Actuator\Single_Actuator_Media\Flexures\working_flexure2_rotation_circle_position\working_flexure2.txt"
-#    plot_trajectory_all_models(file_path, angles)
-# # Replace these with your experimental and theoretical data
-    
-    experimental_x_y = get_pos_model_1(angles)
-    theoretical_x_y = get_pos_video(file_path)
 
-    sys.exit()
+if __name__ == "__main__":
+    file_path = r"C:\Users\Julien\OneDrive - Harvard University\Documents\Fluidic_Brain\Actuator\Single_Actuator_Media\Flexures\working_flexure2_rotation_circle_position\working_flexure2.txt"
+    plot_trajectory_all_models(file_path, angles)
+
+    _, experimental_x, experimental_y = get_pos_video(file_path)
+    theoretical_x1, theoretical_y1 = get_pos_model_1(angles)
+    theoretical_x2, theoretical_y2 = get_pos_model_2(angles)
+    theoretical_x3, theoretical_y3 = get_pos_model_3(angles)
+
     # Create a common set of x-values to resample/interpolate both curves
-    common_x = np.linspace(min(min(experimental_x), min(theoretical_x)),
-                           max(max(experimental_x), max(theoretical_x)), 100)
+    common_x1 = np.linspace(min(min(experimental_x), min(theoretical_x1)),
+                            max(max(experimental_x), max(theoretical_x1)), 10000)
+    print(common_x1)
+
+    # common_x2 = np.linspace(min(min(experimental_x), min(theoretical_x2)),
+    #                         max(max(experimental_x), max(theoretical_x2)), 100)
+    # common_x3 = np.linspace(min(min(experimental_x), min(theoretical_x3)),
+    # max(max(experimental_x), max(theoretical_x3)), 100)
 
     # Interpolate or resample both curves onto the common x-values
-    experimental_interp_y = np.interp(common_x, experimental_x, experimental_y)
-    theoretical_interp_y = np.interp(common_x, theoretical_x, theoretical_y)
+    experimental_interp_y1 = np.interp(
+        common_x1, experimental_x, experimental_y)
+    # experimental_interp_y2 = np.interp(common_x2, experimental_x, experimental_y)
+    # experimental_interp_y3 = np.interp(common_x3, experimental_x, experimental_y)
 
-    # Calculate the error between the two interpolated curves
-    error = experimental_interp_y - theoretical_interp_y
+    theoretical_interp_y1 = np.interp(
+        common_x1, theoretical_x1, theoretical_y1)
+    # theoretical_interp_y2 = np.interp(
+    #     common_x2, theoretical_x2, theoretical_y2)
+    # theoretical_interp_y3 = np.interp(
+    #     common_x3, theoretical_x3, theoretical_y3)
 
-    # Create a figure and axis object
+    # # Calculate the error between the two interpolated curves
+
+    error_1 = abs((experimental_interp_y1 - theoretical_interp_y1) * 100 /
+                  experimental_interp_y1)
+    # error_1 = abs(experimental_interp_y1 - theoretical_interp_y1) / \
+    # (max(experimental_interp_y1) - min(experimental_interp_y1)) * 100
+    # print('Error 1:', error_1)
+    # error_2 = abs(experimental_interp_y2 - theoretical_interp_y2) / \
+    #     (max(experimental_interp_y2) - min(experimental_interp_y2)) * 100
+    # error_3 = abs(experimental_interp_y3 - theoretical_interp_y3) / \
+    #     (max(experimental_interp_y3) - min(experimental_interp_y3)) * 100
+
+    # # Create a figure and axis object
     fig, ax = plt.subplots(figsize=(8, 6))
+    # plt.errorbar(experimental_interp_y, q_measured_list, yerr=q_std_list, fmt='o', capsize=5, label='Measured')
+    # plt.plot(q_set_list, q_set_list, label='Set', linestyle='--')
+    # plt.plot(q_set_fit, q_measured_fit, color='red', label='Fitted Line  (Degree '+str(degree) + ')'
 
-    # Plot the error between the two curves
-    ax.plot(common_x, error, label='Error')
+    # # Plot the error between the two curves
+    ax.plot(common_x1, error_1, label='Error % Model 1')
+    # ax.plot(common_x2, error_2, label='Error % Model 2')
+    # ax.plot(common_x3, error_3, label='Error % Model 3')
 
-    # Set labels and title
+    # # Set labels and title
     ax.set_xlabel("X-values")
-    ax.set_ylabel("Error")
+    ax.set_ylabel("% Error")
     ax.set_title("Error between Experimental and Theoretical Curves")
 
-    # Add a legend
+    # # Add a legend
     ax.legend()
-
     plt.show()
--- Actuator/video_txt_path_export.py
+++ Actuator/video_txt_path_export.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read_trajectory_data(file_path):
+
+def read_point_trajectory_data(file_path):
     time = []
     x_values = []
     y_values = []
@@ -15,9 +16,8 @@ def read_trajectory_data(file_path):
                     t, x, y = map(float, line.split())
                     # print('time, position x, position y:', t, x, y)
                     time.append(t)
-                    if y >2:
-                        x_values.append(-x)
-                        y_values.append(y)
+                    x_values.append(-x)
+                    y_values.append(y)
                 except ValueError:
                     # Skip lines that cannot be converted to float (e.g., headers)
                     pass
@@ -25,10 +25,48 @@ def read_trajectory_data(file_path):
     return time, x_values, y_values
 
 
-def plot_trajectory(file_path):
-    time, x_values, y_values = read_trajectory_data(file_path)
+def read_arrow_trajectory_data(file_path, base_str: str, arrow_str: str,):
+    time_base, base_x_values, base_y_values = [], [], []
+    time_arrow, arrow_x_values, arrow_y_values = [], [], []
+    flag_base, flag_arrow = False, False
+
+    with open(file_path, 'r', encoding='utf-8-sig') as file:
+        lines = file.readlines()
+        print(f"Number of lines read: {len(lines)}")
+        for line in lines:
+            if line.startswith(base_str):
+                flag_base = True
+                flag_arrow = False
+            if line.startswith(arrow_str):
+                flag_base = False
+                flag_arrow = True
+            if line.strip() and not line.startswith('#'):
+                try:
+                    t, x, y = map(float, line.split())
+                    if flag_base:
+                        print('base, time, position x, position y:', t, x, y)
+                        time_base.append(t)
+                        base_x_values.append(x)
+                        base_y_values.append(y)
+                    if flag_arrow:
+                        print('arrow, time, position x, position y:', t, x, y)
+                        time_arrow.append(t)
+                        arrow_x_values.append(x)
+                        arrow_y_values.append(y)
+                except ValueError:
+                    # Skip lines that cannot be converted to float (e.g., headers)
+                    pass
+
+        pos_base = (time_base, base_x_values, base_y_values)
+        pos_arrow = (time_arrow, arrow_x_values, arrow_y_values)
+
+    return pos_base, pos_arrow
+
+
+def plot_point_trajectory(file_path):
+    time, x_values, y_values = read_point_trajectory_data(file_path)
     plt.figure(figsize=(10, 6))
-    plt.plot(x_values,y_values, label='Position', marker='o')
+    plt.plot(x_values, y_values, label='Position', marker='o')
     # plt.plot(time,y_values, label='X Values', marker='o')
     # plt.plot(time, y_values, label='Y Values', marker='o')
     plt.xlabel('x value')
@@ -38,6 +76,41 @@ def plot_trajectory(file_path):
     plt.grid(True)
     plt.show()
 
+
+def plot_arrow_trajectory(fiLe_path, base_str: str, arrow_str: str):
+    pose_base, pose_arrow = read_arrow_trajectory_data(
+        arrow_path, base_str, arrow_str)
+
+    time_base, base_x_values, base_y_values = pose_base
+    time_arrow, arrow_x_values, arrow_y_values = pose_arrow
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(base_x_values, base_y_values, label='Base', marker='o', color='b')
+    plt.plot(arrow_x_values, arrow_y_values,
+             label='Arrow', marker='o', color='r')
+
+    plt.xlabel('X Position')
+    plt.ylabel('Y Position')
+    plt.title('Trajectory')
+    plt.legend()
+    plt.grid(True)
+
+    # Adding arrows between the base and arrow points
+    for i in range(len(base_x_values)):
+        # Base point coordinates
+        arrow_base_point = (base_x_values[i], base_y_values[i])
+        # Arrow point coordinates
+        arrow_end_point = (arrow_x_values[i], arrow_y_values[i])
+        arrow_properties = dict(arrowstyle='->', color='b')  # Arrow properties
+
+        plt.annotate('', xy=arrow_end_point, xytext=arrow_base_point,
+                     arrowprops=arrow_properties)
+
+    plt.show()
+
+
 if __name__ == "__main__":
-    file_path = r"C:\Users\Julien\OneDrive - Harvard University\Documents\Fluidic_Brain\Actuator\Single_Actuator_Media\Flexures\working_flexure2_rotation_circle_position\working_flexure2.txt"
-    plot_trajectory(file_path)
+    # point_path = r"C:\Users\Julien\OneDrive - Harvard University\Documents\Fluidic_Brain\Actuator\Single_Actuator_Media\Flexures\working_flexure2_rotation_circle_position\working_flexure2.txt"
+    # plot_point_trajectory(point_path)
+    arrow_path = r"C:\Users\Julien\OneDrive - Harvard University\Documents\Fluidic_Brain\Actuator\Single_Actuator_Media\Failure\underwater_failure_tube_v\underwater_failure_tube_v.txt"
+    plot_arrow_trajectory(arrow_path, '# pos_blue', '# pos_red')
