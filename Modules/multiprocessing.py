02d05f54e56c7528d733cc4154981aa760f61ea7
Christopher Stocker <julienstocker@icloud.com>
2023-08-07 10:21:33 -0400

    Tests to the soft actuator

--- Modules/multiprocessing.py
+++ Modules/multiprocessing.py
@@ -8,12 +8,13 @@ IDstring = '3-16'
 check_valve_type = 'cv3'
 
 # Pressure Coarse Parameters
-Pmax = 300
-Pmin = -int(Pmax / 4)
+Pmax = 180
+Pmin = 0
 step_size = int((Pmax - Pmin) / 20.)
-plateau_time = 2
+plateau_time = 5
 # plateau_time = 30
-exp_folder = 'node_tube_{:s}_ID_{:s}_{:s}_node_coarse/'.format(Lstring, IDstring, check_valve_type)
+# exp_folder = 'node_tube_{:s}_ID_{:s}_{:s}_node_coarse/'.format(Lstring, IDstring, check_valve_type)
+exp_folder = ('Actuator_in_water_test_0_180')
 voltages_path = r'output/analog_voltages'
 pressure_path = r'output/analog_pressures'
 
@@ -23,30 +24,33 @@ coarse_parameters = {
     "Lstring": Lstring,
     "check_valve_type": check_valve_type,
     "plateau_time": plateau_time,
-    'Pstart': 0,
+    'Pstart': 50,
     "Pmax": Pmax,
     "Pmin": Pmin,
     "step_size": step_size,
     "exp_name": exp_folder,
 }
 
-total_seconds = plateau_time* step_size
+total_seconds = plateau_time * step_size
 total_mins = total_seconds // 60
 print("Total Duration: {}mins {}s".format(total_mins, total_seconds % 60))
 if __name__ == "__main__":
     # Create threads for each script
-    thread3 = threading.Thread(target=Push_Pull_Pressure.process_push_pull_pressure(coarse_parameters))
-    thread2 = threading.Thread(target=SLS_1500.process_sls_1500(total_seconds=total_seconds,file_name=exp_folder))
-    thread1 = threading.Thread(target=Saleae.process_saleae(exp_folder, voltages_path, pressure_path, total_seconds))
+    flg_thread = threading.Thread(
+        target=Push_Pull_Pressure.process_push_pull_pressure(coarse_parameters))
+    sls_thread = threading.Thread(target=SLS_1500.process_sls_1500(
+        total_seconds=total_seconds, file_name=exp_folder))
+    saleae_thread = threading.Thread(target=Saleae.process_saleae(
+        exp_folder, voltages_path, pressure_path, total_seconds))
 
     # Start the threads
-    thread1.start()
-    thread2.start()
-    thread3.start()
+    # saleae_thread.start()
+    # sls_thread.start()
+    flg_thread.start()
 
     # Wait for all threads to finish
-    thread1.join()
-    thread2.join()
-    thread3.join()
+    # saleae_thread.join()
+    # sls_thread.join()
+    flg_thread.join()
 
     print("All scripts processed.")
